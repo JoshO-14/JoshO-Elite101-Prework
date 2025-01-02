ab9831d0397dbc7015fd8cf6a7f2484c0ac1d74d
supremeslacker <amazingjosh101@gmail.com>
2025-01-02 01:04:23 +0000

    update to line 4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Welcome to the soccer shop Chatbot")
 name = input("What is your name? ")
+age = input(f"Hello, {name}, how old are you?")
 choice = input("Select the number that you have questions pertaining to: ")
